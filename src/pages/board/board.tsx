db9ba25237557e92c48214f0057e9cac9a82fe13
Navfront <b3rt@yandex.ru>
2022-11-27 12:30:23 +0300

    Feat: Добавил react-helmet на страницы

--- src/pages/board/board.tsx
+++ src/pages/board/board.tsx
@@ -1,13 +1,20 @@
+import { Helmet } from 'react-helmet'
 import { Link } from 'react-router-dom'
 
 function Board(): JSX.Element {
   return (
-    <div>
-      board
-      <p>
-        <Link to={'/'}>To Main</Link>
-      </p>
-    </div>
+    <>
+      <Helmet>
+        <meta charSet='utf-8' />
+        <title>Главная страница</title>
+      </Helmet>
+      <div>
+        board
+        <p>
+          <Link to={'/'}>To Main</Link>
+        </p>
+      </div>
+    </>
   )
 }
 
--- src/pages/main/main.tsx
+++ src/pages/main/main.tsx
@@ -1,13 +1,20 @@
+import { Helmet } from 'react-helmet'
 import { Link } from 'react-router-dom'
 
 function Main(): JSX.Element {
   return (
-    <div>
-      main
-      <p>
-        <Link to='/board'>To board</Link>
-      </p>
-    </div>
+    <>
+      <Helmet>
+        <meta charSet='utf-8' />
+        <title>Главная страница</title>
+      </Helmet>
+      <div>
+        main
+        <p>
+          <Link to='/board'>To board</Link>
+        </p>
+      </div>
+    </>
   )
 }
 
