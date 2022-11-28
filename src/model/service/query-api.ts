18d6190bae6afc66c4c5b3fdd4485043fe37ce78
Navfront <b3rt@yandex.ru>
2022-11-28 22:10:03 +0300

    Feat: Создал api для работы с fetch и localStorage

---
+++ src/model/service/query-api.ts
@@ -0,0 +1,62 @@
+import { LocalStorageApi } from './local-storage-api'
+
+export interface FetchApiInterface<T> {
+  itemType: string
+  add: (project: T) => Promise<boolean>
+  get: () => Promise<T[]>
+  update: (project: T) => Promise<boolean>
+  delete: (projectId: string) => Promise<boolean>
+}
+
+interface Item {
+  id: string
+  [k: string]: any
+}
+
+export class QueryApi<T extends Item> {
+  localStorageApi: LocalStorageApi
+  fetchApi: FetchApiInterface<T>
+
+  constructor(
+    localStorageApi: LocalStorageApi,
+    fetchApi: FetchApiInterface<T>
+  ) {
+    this.localStorageApi = localStorageApi
+    this.fetchApi = fetchApi
+  }
+
+  async add(project: T): Promise<boolean> {
+    const result = await this.fetchApi.add(project)
+    if (result) {
+      this.localStorageApi.addItem(this.fetchApi.itemType, project)
+      return true
+    }
+    return false
+  }
+
+  async get(): Promise<T[]> {
+    const result = await this.fetchApi.get()
+    if (result.length > 0) {
+      this.localStorageApi.setItems(this.fetchApi.itemType, result)
+    }
+    return result
+  }
+
+  async update(project: T): Promise<boolean> {
+    const result = await this.fetchApi.update(project)
+    if (result) {
+      this.localStorageApi.updateItemById(this.fetchApi.itemType, project)
+      return true
+    }
+    return false
+  }
+
+  async delete(project: T): Promise<boolean> {
+    const result = await this.fetchApi.delete(project.id)
+    if (result) {
+      this.localStorageApi.updateItemById(this.fetchApi.itemType, project, true)
+      return true
+    }
+    return false
+  }
+}
