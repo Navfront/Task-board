5277d7292f4fc5de290d4dea39bf4b44036d3299
Navfront <b3rt@yandex.ru>
2022-11-29 00:45:08 +0300

    Feat: Обработал логику создания проекта

--- src/components/project-editor/project-editor.tsx
+++ src/components/project-editor/project-editor.tsx
@@ -1,6 +1,7 @@
-import { useState } from 'react'
+import { FormEvent, useState } from 'react'
 import { Project } from '../../model/data-types'
 import { useAppDispatch } from '../../redux'
+import { ProjectsApiFacade } from './../../model/service/projects-api-facade'
 
 interface ProjectEditorProps {
   project?: Project
@@ -9,6 +10,7 @@ interface ProjectEditorProps {
 function ProjectEditor({ project }: ProjectEditorProps): JSX.Element {
   const [title, setTitle] = useState(project?.title ?? '')
   const [description, setDescription] = useState(project?.description ?? '')
+  const [isLoading, setIsLoading] = useState(false)
 
   const dispatch = useAppDispatch()
 
@@ -16,13 +18,34 @@ function ProjectEditor({ project }: ProjectEditorProps): JSX.Element {
     dispatch({ type: 'CLOSE_MODAL', payload: { isOpen: false } })
   }
 
+  const onSubmitButtonClick = (event: FormEvent): void => {
+    event.preventDefault()
+    const newProject = {
+      description,
+      title,
+      id: Date.now().toString(),
+      time: null,
+      newComments: 0,
+      userId: 'iAm' // need token or userId
+    }
+    setIsLoading(true)
+    ProjectsApiFacade.projectsQueryApi
+      .add(newProject)
+      .then((ok) => {
+        if (ok) {
+          dispatch({
+            type: 'ADD_PROJECT',
+            project: newProject
+          })
+          setIsLoading(false)
+          dispatch({ type: 'CLOSE_MODAL' })
+        }
+      })
+      .catch(console.log)
+  }
+
   return (
-    <form
-      className='project-editor'
-      onSubmit={(event) => {
-        event.preventDefault()
-      }}
-    >
+    <form className='project-editor' onSubmit={onSubmitButtonClick}>
       <fieldset className='project-editor__fieldset'>
         <label
           htmlFor='project-editor__title-input'
@@ -56,7 +79,11 @@ function ProjectEditor({ project }: ProjectEditorProps): JSX.Element {
           }}
         ></textarea>
         <button type='submit' className='project-editor__button'>
-          {project?.title != null ? 'Edit' : 'Create'}
+          {isLoading
+            ? 'Loading...'
+            : project?.title != null
+              ? 'Edit'
+              : 'Create'}
         </button>
         <button
           type='button'
