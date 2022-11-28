009f7cf6cac217b26d9985b378c9b958658cfdd9
Navfront <b3rt@yandex.ru>
2022-11-29 00:43:18 +0300

    Fix: Типизировал рут редюсер

--- src/redux/index.ts
+++ src/redux/index.ts
@@ -1,8 +1,9 @@
 import { TypedUseSelectorHook, useDispatch, useSelector } from 'react-redux'
 import { store } from './store'
+import { rootReducer } from './reducers/root-reducer'
 
 // Infer the `RootState` and `AppDispatch` types from the store itself
-export type RootState = ReturnType<typeof store.getState>
+export type RootState = ReturnType<typeof rootReducer>
 
 // Inferred type: {posts: PostsState, comments: CommentsState, users: UsersState}
 export type AppDispatch = typeof store.dispatch
