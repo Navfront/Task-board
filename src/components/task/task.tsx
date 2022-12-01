4b45b4b09beb4607067da3d39388b147436c8b75
Navfront <b3rt@yandex.ru>
2022-12-02 00:08:03 +0300

    Feat: Реализовал сворачивание карточки таска

--- src/components/task/task.tsx
+++ src/components/task/task.tsx
@@ -1,5 +1,5 @@
+import React, { useState } from 'react'
 import { columnTitles } from './../board-column/board-column'
-import { useState } from 'react'
 
 interface ITaskProps {
   taskId: string
@@ -17,40 +17,53 @@ interface ITaskProps {
 
 function Task(task: ITaskProps): JSX.Element {
   const [isExpand, setIsExpand] = useState<boolean>(false)
-
+  const expanderRef = React.createRef<HTMLDivElement>()
+  const contentRef = React.createRef<HTMLDivElement>()
   const onExpandClickHandler = (): void => {
     setIsExpand(!isExpand)
+    if (expanderRef.current !== null && contentRef.current !== null) {
+      if (!isExpand) {
+        expanderRef.current.style.height = String(contentRef.current.clientHeight) + 'px'
+      } else {
+        expanderRef.current.style.height = '1px'
+      }
+    }
   }
 
   return (
-    <article className={`task task--priority-${task.priority}`}>
+    <article className={`task task--priority-${task.priority}`} onClick={onExpandClickHandler}>
       <h3 className='task__title'>Title</h3>
       <button
         type='button'
-        className={`task__expand-button ${isExpand ? 'task__expand-button--active' : ''}`}
+        className={`task__expand-button ${!isExpand ? 'task__expand-button--active' : ''}`}
         onClick={onExpandClickHandler}
       >
         {isExpand ? 'Свернуть' : 'Развернуть'}
       </button>
-      <div className={`task__expander ${isExpand ? 'task__expander--active' : ''}`}>
-        <p>
-          <span>Created: {task.createdDate.toDateString()}</span>
-          <span>In work: {task.inWork} ms</span>
-        </p>
+      <div
+        ref={expanderRef}
+        className={`task__expander ${!isExpand ? 'task__expander--active' : ''}`}
+      >
+        <div ref={contentRef} className='task__content'>
+          <p>
+            <span>Created: {task.createdDate.toDateString()}</span>
+            <span>In work: {task.inWork} ms</span>
+          </p>
 
-        <ul className='task__sub-list'>
-          <li className='task__sub-item'>Subtask 1</li>
-          <li className='task__sub-item'>Subtask 2</li>
-          <li className='task__sub-item'>Subtask 3</li>
-        </ul>
+          <ul className='task__sub-list'>
+            <li className='task__sub-item'>Subtask 1</li>
+            <li className='task__sub-item'>Subtask 2</li>
+            <li className='task__sub-item'>Subtask 3</li>
+          </ul>
 
-        <ul className='task__files files'>
-          <li className='files-list__item'>file</li>
-        </ul>
+          <ul className='task__files files'>
+            <li className='files-list__item'>file</li>
+          </ul>
 
-        <p className='task__comments-counter'>
-          Comments: 324 <button type='button'>Read comments</button>
-        </p>
+          <p className='task__comments-counter'>
+            Comments: 324 <button type='button'>Read comments</button>
+          </p>
+        </div>
       </div>
     </article>
   )
