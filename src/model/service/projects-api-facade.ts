10b7f3640b8244fd2409dfd56e8f76221aa22b77
Navfront <b3rt@yandex.ru>
2022-12-04 13:07:19 +0300

    Refact: Отрефакторил ProjectsApiFacade

--- src/model/service/projects-api-facade.ts
+++ src/model/service/projects-api-facade.ts
@@ -3,11 +3,29 @@ import { FakeProjectsFetch } from './fake-projects-fetch'
 import { LocalStorageApi } from './local-storage-api'
 import { QueryApi } from './query-api'
 
+/**
+ * Класс Singleton-Facade по работе с queryapi
+ */
 export class ProjectsApiFacade {
-  static projectsQueryApi = new QueryApi<IProject, 'projects'>(
-    LocalStorageApi.getInstance(),
-    new FakeProjectsFetch()
-  )
+  private static INSTANCE: QueryApi<IProject, 'projects'> | undefined
+  queryApi: QueryApi<IProject, 'projects'>
+  localStorage: LocalStorageApi
 
-  hello(): void {}
+  private constructor(queryApy: QueryApi<IProject, 'projects'>, localStorage: LocalStorageApi) {
+    this.queryApi = queryApy
+    this.localStorage = localStorage
+  }
+
+  /**
+   * Возвращает инстанс ProjectsApiFacade
+   */
+  static get projectsQueryApi(): QueryApi<IProject, 'projects'> {
+    if (this.INSTANCE === undefined) {
+      this.INSTANCE = new QueryApi<IProject, 'projects'>(
+        LocalStorageApi.getInstance(),
+        new FakeProjectsFetch()
+      )
+    }
+    return this.INSTANCE
+  }
 }
